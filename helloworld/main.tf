a9c94e2c38ea85746856114626eb993921adf4b9
choisungwook <kgg1959@naver.com>
2023-06-15 09:24:38 +0900

    리팩토링: fmt

--- helloworld/main.tf
+++ helloworld/main.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     # reference: https://registry.terraform.io/providers/hashicorp/local/2.4.0
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.4.0"
     }
   }
 }
 
 # reference: https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file.html
-
 resource "local_file" "demo" {
   content  = "hi. how are you?"
   filename = "helloworld.txt"
