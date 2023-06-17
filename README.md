4c376120f24cdce2d666a4bcbf6f2f8ed24d4e41
choisungwook <kgg1959@naver.com>
2023-06-17 14:47:09 +0900

    #2 submodule에 있는 테라폼 코드를 실행하는지 확인하는 예제

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 # 목차
 [helloworld](./helloworld/)
+[서브모듈 실행안되는 예제](./ignore_submodule/)
---
+++ ignore_submodule/submodule/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    # reference: https://registry.terraform.io/providers/hashicorp/local/2.4.0
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+# reference: https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file.html
+resource "local_file" "demo" {
+  content  = "hi. how are you?"
+  filename = "helloworld.txt"
+}
