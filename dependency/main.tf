ed1ca7026a7e5bc1008beff5f14bffd87ceb09e7
choisungwook <kgg1959@naver.com>
2023-06-17 15:03:17 +0900

    #3 테라폼 실행순서 예제 추가

---
+++ dependency/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+resource "local_file" "first" {
+  content  = "dependency"
+  filename = "first.txt"
+}
+
+resource "local_file" "second" {
+  content  = local_file.first.content
+  filename = "second.txt"
+}
+
+resource "local_file" "third" {
+  content  = "not dependency"
+  filename = "third.txt"
+}
