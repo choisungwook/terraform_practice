c8655b50237275b2c0e601c3a72550258d94a99a
choisungwook <kgg1959@naver.com>
2023-10-01 21:16:16 +0900

    Modify: nat_gw가 사용되는 private subnet 이름 변경

--- eks/main.tf
+++ eks/main.tf
@@ -1,29 +1,43 @@
-# module "vpc" {
-#   source = "./module/vpc"
+module "vpc" {
+  source = "./module/vpc"
 
-#   vpc_cidr = "10.0.0.0/16"
-#   public_subnets = {
-#     "subnet_a" = {
-#       cidr = "10.0.0.0/24",
-#       az   = "ap-northeast-2a",
-#       tags = {
-#         Name = "public subnet a"
-#       }
-#     }
-#   }
-#   private_subnets = {
-#     "subnet_a" = {
-#       cidr = "10.0.100.0/24",
-#       az   = "ap-northeast-2a",
-#       tags = {
-#         Name = "private subnet a"
-#       }
-#     }
-#   }
-# }
-
-module "eks" {
-  source = "./module/eks"
-
-  eks-name = "eks-from-terraform"
+  vpc_cidr = "10.0.0.0/16"
+  public_subnets = {
+    "subnet_a1" = {
+      cidr = "10.0.10.0/24",
+      az   = "ap-northeast-2a",
+      tags = {
+        Name = "public subnet a1"
+      }
+    },
+    "subnet_b1" = {
+      cidr = "10.0.11.0/24",
+      az   = "ap-northeast-2c",
+      tags = {
+        Name = "public subnet c1"
+      }
+    }
+  }
+  private_subnets = {
+    "subnet_a1" = {
+      cidr = "10.0.100.0/24",
+      az   = "ap-northeast-2a",
+      tags = {
+        Name = "private subnet a1"
+      }
+    },
+    "subnet_b1" = {
+      cidr = "10.0.101.0/24",
+      az   = "ap-northeast-2c",
+      tags = {
+        Name = "private subnet c1"
+      }
+    }
+  }
 }
+
+# module "eks" {
+#   source = "./module/eks"
+
+#   eks-name = "eks-from-terraform"
+# }
