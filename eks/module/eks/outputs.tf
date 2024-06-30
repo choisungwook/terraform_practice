0726f70200a4bf579f7713f29181bd1bbfa2a2c2
choisungwook <kgg1959@naver.com>
2024-06-30 11:07:51 +0900

    KAN-185 Add oidc provider output of EKS moduile

--- eks/module/eks/outputs.tf
+++ eks/module/eks/outputs.tf
@@ -7,3 +7,13 @@ output "eks_cluster_arn" {
   description = "eks cluster arn"
   value       = aws_eks_cluster.main.arn
 }
+
+output "oidc_provider_arn" {
+  description = "oidc provider arn"
+  value       = aws_iam_openid_connect_provider.main.arn
+}
+
+output "oidc_provider_url" {
+  description = "oidc provider arn"
+  value       = aws_iam_openid_connect_provider.main.url
+}
