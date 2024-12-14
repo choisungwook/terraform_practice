2a2a2030ca596afbaacf591790bb20f467f5a52e
choisungwook <9348148+choisungwook@users.noreply.github.com>
2024-12-15 02:21:43 +0900

    #37 Refactor EKS module variables

--- eks/main.tf
+++ eks/main.tf
@@ -3,39 +3,9 @@ module "vpc" {
 
   eks_cluster_name = var.eks_cluster_name
 
-  vpc_cidr = "10.0.0.0/16"
-  public_subnets = {
-    "subnet_a1" = {
-      cidr = "10.0.10.0/24",
-      az   = "ap-northeast-2a",
-      tags = {
-        Name = "public-subnet-a1"
-      }
-    },
-    "subnet_b1" = {
-      cidr = "10.0.11.0/24",
-      az   = "ap-northeast-2c",
-      tags = {
-        Name = "public-subnet-c1"
-      }
-    }
-  }
-  private_subnets = {
-    "subnet_a1" = {
-      cidr = "10.0.100.0/24",
-      az   = "ap-northeast-2a",
-      tags = {
-        Name = "private-subnet-a1"
-      }
-    },
-    "subnet_b1" = {
-      cidr = "10.0.101.0/24",
-      az   = "ap-northeast-2c",
-      tags = {
-        Name = "private-subnet-c1"
-      }
-    }
-  }
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
 
 module "eks" {
--- eks/variables.tf
+++ eks/variables.tf
@@ -35,6 +35,18 @@ variable "managed_node_groups" {
     max_size        = number
     min_size        = number
   }))
+  default = {
+    "ondemand-group-a" = {
+      node_group_name = "ondemand-group-a",
+      instance_types  = ["t3.medium"],
+      capacity_type   = "SPOT",
+      release_version = "" #latest
+      disk_size       = 20
+      desired_size    = 3,
+      max_size        = 3,
+      min_size        = 3
+    }
+  }
 }
 
 variable "assume_role_arn" {
@@ -44,4 +56,60 @@ variable "assume_role_arn" {
 variable "enable_amp" {
   type    = bool
   default = false
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "VPC public subnets"
+  type = map(object({
+    cidr = string
+    az   = string
+    tags = map(string)
+  }))
+  default = {
+    "subnet_a1" = {
+      cidr = "10.0.10.0/24",
+      az   = "ap-northeast-2a",
+      tags = {
+        Name = "public-subnet-a1"
+      }
+    },
+    "subnet_b1" = {
+      cidr = "10.0.11.0/24",
+      az   = "ap-northeast-2c",
+      tags = {
+        Name = "public-subnet-c1"
+      }
+    }
+  }
+}
+
+variable "private_subnets" {
+  description = "VPC private_subnets"
+  type = map(object({
+    cidr = string
+    az   = string
+    tags = map(string)
+  }))
+  default = {
+    "subnet_a1" = {
+      cidr = "10.0.100.0/24",
+      az   = "ap-northeast-2a",
+      tags = {
+        Name = "private-subnet-a1"
+      }
+    },
+    "subnet_b1" = {
+      cidr = "10.0.101.0/24",
+      az   = "ap-northeast-2c",
+      tags = {
+        Name = "private-subnet-c1"
+      }
+    }
+  }
 }
