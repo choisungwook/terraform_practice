94d24eb29762c298427154bc70792ebc9bfc9170
choisungwook <9348148+choisungwook@users.noreply.github.com>
2024-12-31 01:47:32 +0900

    Split aws_eks_addons to before_compute, after_compute (#98)

--- eks/module/eks/addons.tf
+++ eks/module/eks/addons.tf
@@ -1,8 +1,31 @@
-resource "aws_eks_addon" "addons" {
-  for_each = { for addon in var.eks_addons : addon.name => addon }
+resource "aws_eks_addon" "before_compute" {
+  for_each = { for addon in var.eks_addons : addon.name => addon if addon.before_compute }
 
   cluster_name         = aws_eks_cluster.main.id
   addon_name           = each.value.name
   addon_version        = each.value.version
   configuration_values = each.value.configuration_values
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
+}
+
+resource "aws_eks_addon" "after_compute" {
+  for_each = { for addon in var.eks_addons : addon.name => addon if !addon.before_compute }
+
+  cluster_name         = aws_eks_cluster.main.id
+  addon_name           = each.value.name
+  addon_version        = each.value.version
+  configuration_values = each.value.configuration_values
+
+  depends_on = [aws_eks_node_group.main]
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
