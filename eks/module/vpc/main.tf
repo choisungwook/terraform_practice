2b9c9296668a3a0feb701dff66a71337ac24dca6
choisungwook <9348148+choisungwook@users.noreply.github.com>
2025-03-13 23:37:45 +0900

    Add map_public_ip_on_launch to public EKS subnet

--- eks/module/vpc/main.tf
+++ eks/module/vpc/main.tf
@@ -9,9 +9,10 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   for_each = var.public_subnets
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value["cidr"]
-  availability_zone = each.value["az"]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value["cidr"]
+  availability_zone       = each.value["az"]
+  map_public_ip_on_launch = true
 
   tags = merge({
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
