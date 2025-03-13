5bdeb1217a606854db43b05db9f74d7c588b019d
choisungwook <9348148+choisungwook@users.noreply.github.com>
2025-03-13 23:27:37 +0900

    Create public subnet outputs in EKS modules (#99)

--- eks/module/vpc/outputs.tf
+++ eks/module/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "private_subnets_ids" {
   description = "private subnets"
   value       = [for private_subnet in aws_subnet.private : private_subnet.id]
 }
+
+output "public_subnets_ids" {
+  description = "private subnets"
+  value       = [for public_subnets in aws_subnet.public : public_subnets.id]
+}
